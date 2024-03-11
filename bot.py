2dcdd3a18e59d0fa5a1ef28ad94ceb75d6e11a40
Samuel7311 <147135328+Samuel7311@users.noreply.github.com>
2024-03-11 14:27:48 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,31 +1,39 @@
 import requests
 import json
 import time
-def send_data_to_telegram():
-    with open('flucc/flucc/spiders/oFlucc.json', "r", encoding='utf-8') as f:
-        data = json.load(f)
-        for item in data:
-            link = item['Link']
-            event = item['Event']
-            date = item['Date']
+from datetime import date
 
-            # Construct message
-            message = f"Event: {event}\nDate: {date}\nLink: {link}"
+def send_data_to_telegram():
+    # List of JSON files
+    json_files = ['daswerk/erasmus/spiders/oDaswerk.json', 'flex/flex/spiders/oFlex.json',
+                  'flucc/flucc/spiders/oFlucc.json', 'goabase/goabase/spiders/oGoabase.json',
+                  'goodnight/goodnight/spiders/oGoodnight.json']
+
+    for json_file in json_files:
+        # Load JSON file
+        with open(json_file, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+
+    today = date.today()
+    today_string = today.strftime("%d-%m-%Y")
+
+    for item in data:
+        link = item['Link']
+        event = item['Title']
+        event_date = item['Date']
+
+        if today_string == event_date:
+            message = f"Title: {event}\nDate: {event_date}\nLink: {link}"
 
             # Send message to Telegram
             response = requests.post(
                 'https://api.telegram.org/bot7071671373:AAF0U60pGQTFOlkppeyaxkhXUbM-cs-1qdU/sendMessage',
                 json={'chat_id': '-4176067898', 'text': message}
             )
-
-            # Rewrite JSON file without processed line
-            with open('flucc/flucc/spiders/oFlucc.json', "w", encoding='utf-8') as json_file:
-                json.dump(data, json_file, indent=4)
-
             # Pause between sending messages
-            time.sleep(2)
+            time.sleep(1)
 
 if __name__ == "__main__":
     send_data_to_telegram()
 
-    
+
